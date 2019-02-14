f79990b10c09bba678309928ab38b2d9f889f917
tmorgan17 <thomas.w.morgan@gmail.com>
2019-02-14 08:31:56 -0500

    hi to dennis

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome World!</h1>
+          <h1 className="App-title">Welcome Dennis!</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
